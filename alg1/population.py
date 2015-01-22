863d7944ebeb48c8ec390c4645b489f6ec68901e
hawkerpl <m.m.hawker@gmail.com>
2015-01-22 16:41:06 +0100

    lev8 done

--- alg1/population.py
+++ alg1/population.py
@@ -24,35 +24,6 @@ class Population:
             self.fitness_table.append(self.pop[i+1].fitness)
         return self.fitness_table
 
-    def selection(self):
-        survived_table = []
-        for i, ind in enumerate(self.pop):
-            if ind.fitness:
-                survived_table.append(ind)
-                self.selected_indexes.append(i)
-        self.selected = survived_table
-        return self.selected_indexes, self.selected
-
-    def breed(self):
-        new_population = []
-        for i in xrange(0, len(self.selected), 2):
-            child1, child2 = self.selected[i].breed_with(self.selected[i+1])
-            child3, child4 = self.selected[i].breed_with(self.selected[i+1])
-            new_population.append(child1)
-            new_population.append(child2)
-            new_population.append(child3)
-            new_population.append(child4)
-        self.pop = new_population
-        self.selected = []
-        self.selected_indexes = []
-        self.fitness_table = []
-        return new_population
-
-    def evolve(self):
-        self.evaluate()
-        self.selection()
-        self.breed()
-
     def __str__(self):
         result = ""
         for i, ind in enumerate(self.pop):
